97e1c9396524d76649d7a677bab9e14e48a79eaa
Anton Prokhorov <betrayer11@gmail.com>
2016-12-28 21:31:44 +0700

    Replace fabric to invoke

---
+++ {{cookiecutter.project_slug}}/tasks.py
@@ -0,0 +1,25 @@
+from invoke import task
+
+
+@task
+def compile(ctx, env='local'):
+    """Compile *.in dependencies to *.txt file"""
+    ctx.run(f'pip-compile requirements/{env}.in')
+
+
+@task
+def install(ctx, env='local'):
+    """Install dependencies from *.txt file"""
+    ctx.run(f'pip install -r requirements/{env}.txt')
+
+
+@task
+def start_db(ctx, daemon=False):
+    """Start postgres container"""
+    d = '-d' if daemon else ''
+    ctx.run(f'docker-compose up {d} postgres')
+
+
+@task
+def stop_db(ctx):
+    ctx.run('docker-compose stop postgres')
